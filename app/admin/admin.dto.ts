3dad4f9fa662e4da14c5e25f03dcf0f7822000bf
karshdev <sharmaaakarsh332@gmail.com>
2025-01-18 00:34:56 +0530

    added

--- app/admin/admin.dto.ts
+++ app/admin/admin.dto.ts
@@ -3,7 +3,7 @@ import { type BaseSchema } from "../common/dto/base.dto";
 
 export interface IAdmin extends BaseSchema {
         email: string;
-        active?: boolean;
+        active?: boolean
         role:string;
         password: string
 }
